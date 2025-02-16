8ffd1f8a51a8844a9f133585d80eaff3e38b80f1
Bppatkar <bhanupratappatkar777@gmail.com>
2025-02-16 09:50:42 +0530

    finally loops ended and functions start in javascript basic

---
+++ my_learning/javascript/function.js
@@ -0,0 +1,18 @@
+//! functions
+
+/* function orderTea(teaTypes) {
+  function confirmOrder() {
+    return "Order confirmed for chai";
+  }
+  return confirmOrder();
+}
+
+let tea = orderTea("lemon");
+console.log(tea);
+ */
+
+/* let calculateTotal = (price, quantity) => price * quantity;
+
+let totalCost = calculateTotal(220, 5);
+console.log(totalCost);
+ */
