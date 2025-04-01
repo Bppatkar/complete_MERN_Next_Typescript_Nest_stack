3d38c421a03613597de1375eb1f3e4383f41a84a
Bppatkar <bhanupratappatkar777@gmail.com>
2025-04-01 11:31:38 +0530

    taking user input done and redirecting request done

--- my_learning/ME_N/Node_js/first.js
+++ my_learning/ME_N/Node_js/first.js
@@ -6,6 +6,7 @@
 // Node.js is a server-side JavaScript runtime environment, which means it can be used to create server-side applications. */
 
 //? _______________________________________________________
+
 /* //! file system module
 
 // const fs = require("fs");
@@ -76,7 +77,7 @@
 //* 3) https - Handle HTTPS requests and responses. (more secure)
 //* 4) path - Work with file paths and directories. (file handling)
 //* 5) path.os - Access operating system information. (operating system related utility methods and properties)
-//* 6) url - Work with URLs.
+//* 6) url.toLocaleLowerCase() - Work with URLs.
 //* 7) net - Create and manage network connections.
 //* 8) stream - Work with streams of data.
 //* 9) events - Handle events and event emitters. (event-driven programming)
@@ -98,8 +99,9 @@ server.listen(3000, () =>
 ); */
 
 //? _______________________________________________________
-//! Response Object
-/* 
+
+/*  //! Response Object
+
 const http = require("http");
 
 const server = http.createServer((req, res) => {
@@ -116,3 +118,103 @@ server.listen(3000, () =>
 ); */
 
 //? _______________________________________________________
+
+/* //! Routing Request
+
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+
+  let url = req.url;
+  if (url.toLowerCase() === "/") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Welcome to HOMEPAGE 🏡");
+  } else if (url.toLowerCase() === "/about") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Welcome to About page 📚");
+  } else if (url.toLowerCase() === "/profile") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Welcome to admin dashboard 👤");
+  } else {
+    res.statusCode = 404;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("404: Page not Found!! ❌");
+  }
+});
+server.listen(3000, () =>
+  console.log(`server is running at http://localhost:3000`)
+);
+ */
+
+//? _______________________________________________________
+
+/* //! taking user input
+
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-type", "text-html");
+  res.write("<html>");
+  res.write("<head><title>User Input Form</title></head>");
+  res.write("<body>");
+  res.write('<form action="/submit" method="POST">');
+  res.write('<label for="name">Name:</label>');
+  res.write('<input type="text" id="name" name="name" required><br><br>');
+  res.write('<label for="gender">Gender:</label>');
+  res.write('<input type="radio" id="male" name="gender" value="male">');
+  res.write('<label for="male">Male</label>');
+  res.write('<input type="radio" id="female" name="gender" value="female">');
+  res.write('<label for="female">Female</label><br><br>');
+  res.write('<button type="submit">Submit</button>');
+  res.write("</form>");
+  res.write("</body>");
+  res.write("</html>");
+  res.end();
+});
+server.listen(3000, () =>
+  console.log(`server is running at http://localhost:3000`)
+); */
+//* after submitting form go to network tab and see the output in payload or response tab
+
+//? _______________________________________________________
+
+//! redirecting request (using fs module)
+//using same code as above
+
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method, req.headers);
+  if (req.url === "/") {
+    res.setHeader("Content-type", "text-html");
+    res.write("<html>");
+    res.write("<head><title>User Input Form</title></head>");
+    res.write("<body>");
+    res.write('<form action="/submit" method="POST">');
+    res.write('<label for="name">Name:</label>');
+    res.write('<input type="text" id="name" name="name" required><br><br>');
+    res.write('<label for="gender">Gender:</label>');
+    res.write('<input type="radio" id="male" name="gender" value="male">');
+    res.write('<label for="male">Male</label>');
+    res.write('<input type="radio" id="female" name="gender" value="female">');
+    res.write('<label for="female">Female</label><br><br>');
+    res.write('<button type="submit">Submit</button>');
+    res.write("</form>");
+    res.write("</body>");
+    res.write("</html>");
+    res.end();
+  } else if (req.method == "POST" && req.url.toLowerCase() === "/submit") {
+    fs.writeFileSync("data.txt", 'bhanu pratap patkar'); //creating new file data.txt
+    
+    res.statusCode = 301; // redirecting to home page 
+    res.setHeader("Location", "/"); // redirecting to home page
+    res.end();
+  }
+});
+server.listen(3000, () =>
+  console.log(`server is running at http://localhost:3000`)
+);
