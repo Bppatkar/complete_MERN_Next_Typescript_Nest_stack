b4289f155858f395383e24516bcdce7904348bd7
Bppatkar <bhanupratappatkar777@gmail.com>
2025-04-03 16:14:38 +0530

    express js start

---
+++ my_learning/ME_N/Express_js/newFile.js
@@ -0,0 +1,4 @@
+//! What is Express.js
+// Express js is a minimal and flexible web application framework for nodejs
+// it provide a robust set of features for building single-page, multiple-page, and hybrid web applications.
+// Express js simplifies server-side coding by providing a layer of fundamental web application features.
