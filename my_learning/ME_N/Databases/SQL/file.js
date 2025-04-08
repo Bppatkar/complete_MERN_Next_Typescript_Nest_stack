1cfe53ac2818a66527f9181980369328cd937ae5
Bppatkar <bhanupratappatkar777@gmail.com>
2025-04-08 13:35:46 +0530

    learning about sql  and noSql databases

---
+++ my_learning/ME_N/Databases/SQL/file.js
@@ -0,0 +1,19 @@
+//! SQl
+//? SQL is a structured query language used to interact with databases.
+//* it is a language to interact with database and perform operations on it
+
+// if it is structured then in which form data will stored
+//? (1) Relational Model - Organize data into tables with rows and columns
+//? (2) Fixed Schema - Require a predefined schema; the structure of the data must be known in advance.
+
+//! properties 
+//? 1) Vertical Scalability - Typically scaled by increasing the resources of a single server. (scaling up)
+//? 2) Relationships - Tables can have multiple types of relationships.
+
+//! When to Use SQL Databases
+//*  When data integrity and relationships are critical (e.g., banking systems).
+//* When the schema is stable and predefined.
+
+//! When Not to Use SQL
+//* For unstructured or rapidly changing data (use NoSQL instead).
+//* When horizontal scaling is a priority (e.g., big data applications).
