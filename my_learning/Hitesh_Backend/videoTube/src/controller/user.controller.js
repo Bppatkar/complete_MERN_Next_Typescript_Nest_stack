c83f9c58260130b9e8daedddb6807c033bc91b73
Bppatkar <bhanupratappatkar777@gmail.com>
2025-04-21 18:15:45 +0530

    working on rest of controller

--- my_learning/Hitesh_Backend/videoTube/src/controller/user.controller.js
+++ my_learning/Hitesh_Backend/videoTube/src/controller/user.controller.js
@@ -358,4 +358,71 @@ const logoutUser = asyncHandler(async (req, res) => {
   );
 }} */
 
-export { registerUser, loginUser, refreshAccessToken, logoutUser };
+const changedCurrentPassword = asyncHandler(async (req, res) => {
+  const { oldPassword, newPassword } = req.body;
+
+  const user = await User.findById(req.user?._id);
+  const isPasswordCorrect = await user.isPasswordCorrect(oldPassword);
+
+  if (!isPasswordCorrect) {
+    throw new ApiError(400, "Old password is incorrect");
+  }
+
+  user.password = newPassword;
+  await user.save({ validateBeforeSave: false });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Password changed successfully"));
+});
+
+const getCurrentUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (!user) throw new ApiError(400, "User not found");
+  return res.status(200).json(new ApiResponse(200, { user }, "User Found"));
+});
+
+const updateAccountDetails = asyncHandler(async (req, res) => {
+  const { fullName, email } = req.body;
+
+  if (!fullName || !email) {
+    throw new ApiError(400, "All fields are required");
+  }
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: {
+        fullName,
+        email: email,
+      },
+    },
+    {
+      new: true,
+    }
+  ).select("-password");
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Account details updated successfully"));
+});
+
+const updateUserAvatar = asyncHandler(async (req, res) => {});
+
+const updateUserCoverImage = asyncHandler(async (req, res) => {});
+
+const getUserChannelProfile = asyncHandler(async (req, res) => {});
+
+const getWatchHistory = asyncHandler(async (req, res) => {});
+
+export {
+  registerUser,
+  loginUser,
+  refreshAccessToken,
+  logoutUser,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
+  changedCurrentPassword,
+};
