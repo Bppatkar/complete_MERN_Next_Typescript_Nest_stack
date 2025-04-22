29f53d2a40770b01c0ded2e37fbd302b5dfcfd19
Bppatkar <bhanupratappatkar777@gmail.com>
2025-04-22 10:35:39 +0530

    controllers update

--- my_learning/Hitesh_Backend/videoTube/src/controller/user.controller.js
+++ my_learning/Hitesh_Backend/videoTube/src/controller/user.controller.js
@@ -397,7 +397,7 @@ const updateAccountDetails = asyncHandler(async (req, res) => {
       },
     },
     {
-      new: true,
+      new: true, // MIMP - this will allow to update information to come
     }
   ).select("-password");
   return res
@@ -405,9 +405,58 @@ const updateAccountDetails = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, user, "Account details updated successfully"));
 });
 
-const updateUserAvatar = asyncHandler(async (req, res) => {});
+const updateUserAvatar = asyncHandler(async (req, res) => {
+  const avatarLocalPath = req.file?.path; // directly decoding the path [req.file.path refers to the temporary location where the uploaded file has been stored on the server]
 
-const updateUserCoverImage = asyncHandler(async (req, res) => {});
+  if (!avatarLocalPath) throw new ApiError(400, "Avatar file is missing");
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  if (!avatar.url)
+    throw new ApiError(
+      500,
+      "Something went wrong while uploading avatar on cloudinary"
+    );
+
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: {
+        avatar: avatar.url,
+      },
+    },
+    {
+      new: true, // MIMP - this will allow to update information to come
+    }
+  ).select("-password");
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Avatar updated successfully"));
+});
+
+const updateUserCoverImage = asyncHandler(async (req, res) => {
+  const coverImageLocalPath = req.file?.path; // directly decoding the path [req.file.path refers to the temporary location where the uploaded file has been stored on the server]
+  if (!coverImageLocalPath)
+    throw new ApiError(400, "Cover Image file is missing");
+
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+  if (!coverImage.url)
+    throw new ApiError(
+      500,
+      "Something went wrong while uploading cover image on cloudinary"
+    );
+
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: { coverImage: coverImage.url },
+    },
+    {
+      new: true, // MIMP - this will allow to update information to come}
+    }
+  ).select("-password");
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Cover Image updated successfully"));
+});
 
 const getUserChannelProfile = asyncHandler(async (req, res) => {});
 
