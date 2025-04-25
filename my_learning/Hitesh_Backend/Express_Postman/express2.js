ad80e3381bef6fb01b1f130322d16063deb5ec07
Bppatkar <bhanupratappatkar777@gmail.com>
2025-04-25 08:18:16 +0530

    some thory detail

--- my_learning/Hitesh_Backend/Express_Postman/express2.js
+++ my_learning/Hitesh_Backend/Express_Postman/express2.js
@@ -7,7 +7,7 @@ const localhost = "127.0.0.1";
 // we are accepting data from frontend side ok
 // using middleware
 app.use(express.json()); // receiving json data from frontend
-app.use(express.urlencoded({ extended: true })); // receiving data from url which is in Coded form ok
+app.use(express.urlencoded({ extended: true })); // receiving data from any "FORM" and that data come from url which is in Coded form ok
 // means any data we receive in json formate we accept that
 
 //* we make simple application which stores my data in an array and perform a CRUD operation
