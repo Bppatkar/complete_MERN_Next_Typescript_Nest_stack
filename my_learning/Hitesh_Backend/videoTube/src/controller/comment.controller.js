cbcdbb44b46e0bfe57cc2ebec14656b25b85161c
Bppatkar <bhanupratappatkar777@gmail.com>
2025-04-30 11:11:32 +0530

    comment.controller done

--- my_learning/Hitesh_Backend/videoTube/src/controller/comment.controller.js
+++ my_learning/Hitesh_Backend/videoTube/src/controller/comment.controller.js
@@ -10,43 +10,181 @@ const getVideoComments = asyncHandler(async (req, res) => {
   const { page = 1, limit = 10 } = req.query;
 
   if (!videoId || !isValidObjectId(videoId)) {
-    throw new ApiError(400, "getVideoComments :: video Id is not valid");
+    throw new ApiError(400, "getVideoComments :: Video Id is not valid");
   }
 
-  const comment = await Comment.aggregate([
-    {$match : {
-      video: mongoose.Types.ObjectId(videoId),
-      }},
-      {
-        $lookup:{
-          form: "users",
-          localField: "owner",
-          foreignField: "_id",
-          as: "owner",
-          pipeline:[
-            {$project:{
-              _id: 1,
-              userName: 1,
-              avatar:1,
-              fullName:1,
-            }}
-          ]
-        }
+  const comments = await Comment.aggregate([
+    {
+      $match: {
+        video: new mongoose.Types.ObjectId(videoId),
       },
-      //  this step converts the owner array into an object
-  ]})
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "owner",
+        pipeline: [
+          {
+            $project: {
+              _id: 1,
+              username: 1,
+              avatar: 1,
+              fullName: 1,
+            },
+          },
+        ],
+      },
+    },
+    // This step converts the owner array into an object
+    {
+      $unwind: "$owner",
+    },
+    {
+      $lookup: {
+        from: "likes",
+        localField: "_id",
+        foreignField: "comment",
+        as: "likes",
+      },
+    },
+    {
+      $addFields: {
+        likesCount: {
+          $size: "$likes",
+        },
+        isLiked: {
+          $cond: {
+            if: { $in: [req.user?._id, "$likes.likedBy"] },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        _id: 1,
+        createdAt: 1,
+        username: 1,
+        avatar: 1,
+        likesCount: 1,
+        isLiked: 1,
+        content: 1,
+        owner: 1,
+      },
+    },
+    {
+      $skip: (parseInt(page) - 1) * limit,
+    },
+    {
+      $limit: parseInt(limit),
+    },
+  ]);
+
+  const totalComments = await Comment.countDocuments({ video: videoId });
+  // console.log("COMMENTS", comments);
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { comments, totalComments },
+        "Comments fetched successfully"
+      )
+    );
 });
 
 const addComment = asyncHandler(async (req, res) => {
   // TODO: add a comment to a video
+  const { content } = req.body;
+  const { videoId } = req.params;
+
+  if (!content?.trim()) throw new ApiError(400, "Comment content is required");
+
+  if (!videoId || !isValidObjectId(videoId)) {
+    throw new ApiError(400, "addComment :: Video Id is not valid");
+  }
+
+  const comment = await Comment.create({
+    content,
+    video: mongoose.Types.ObjectId(videoId),
+    owner: mongoose.Types.ObjectId(req.user?._id),
+  });
+
+  if (!comment) throw new ApiError(500, "Failed to add comment");
+  //  console.log("COMMENT", comment);
+
+  return res.status(201).json({
+    success: true,
+    message: "Comment added successfully",
+    comment,
+  });
 });
 
 const updateComment = asyncHandler(async (req, res) => {
   // TODO: update a comment
+  const { content } = req.body;
+  const { commentId } = req.params;
+
+  if (!content?.trim()) throw new ApiError(400, "Comment content is required");
+
+  if (!commentId || !isValidObjectId(commentId))
+    throw new ApiError(400, "updateComment :: Comment Id is not valid");
+
+  const comment = await Comment.findById(commentId);
+  if (req.user?._id.toString() !== comment.owner.toString())
+    throw new ApiError(
+      401,
+      "updateComment :: You do not have permission to perform this action"
+    );
+
+  const updatedComment = await Comment.findByIdAndUpdate(
+    commentId,
+    {
+      $set: { content },
+    },
+    {
+      new: true,
+    }
+  );
+
+  if (!updateComment) throw new ApiError(500, "Failed to update comment");
+  return res.status(200).json({
+    success: true,
+    message: "Comment updated successfully",
+    updateComment,
+  });
 });
 
 const deleteComment = asyncHandler(async (req, res) => {
   // TODO: delete a comment
+  const { commentId } = req.params;
+
+  if (!commentId || !isValidObjectId(commentId))
+    throw new ApiError(400, "deleteComment :: Comment Id is not valid");
+
+  const comment = await Comment.findById(commentId);
+
+  if (!comment) throw new ApiError(404, "Comment not found");
+
+  if (req.user?._id.toString() !== comment.owner.toString())
+    throw new ApiError(
+      401,
+      "deleteComment :: You do not have permission to perform this action"
+    );
+
+  const deleteComment = await Comment.findByIdAndDelete(commentId);
+  if (!deleteComment) throw new ApiError(500, "Failed to delete comment");
+  console.log("deleteComment", deleteComment);
+
+  return res.status(200).json({
+    success: true,
+    message: "Comment deleted successfully",
+    deleteComment,
+  });
 });
 
 export { getVideoComments, addComment, updateComment, deleteComment };
