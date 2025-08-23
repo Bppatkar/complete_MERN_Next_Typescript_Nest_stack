2c901db155d23b26357f33506de152c2a637d1f8
Bhanu Pratap Patkar <bhanupratappatkar777@gmail.com>
2025-08-23 12:00:18 +0530

    array and tuple done

---
+++ my_learning/typeScript_tutorial/arrayDT.ts
@@ -0,0 +1,10 @@
+// what is array data type
+//? collection of similar data types and it is not a user defined data type , it is a pre defined data type
+// how to use
+// example
+let numbers: number[] = [1, 2, 3, 4];
+let names: Array<string> = ['Alice', 'Bob', 'Charlie'];
+let newNum: Array<number> = [5, 43, 6, 4];
+newNum.push(45);
+console.log(newNum);
+// where we ca use it
---
+++ my_learning/typeScript_tutorial/tupleDT.ts
@@ -0,0 +1,12 @@
+// array vs tuppe
+//? we can put same things in array but in tupple we can put diff types of data
+//? tuple is a fixed length ordered collection of diffrent types
+//?  each element in a tuple has a specific type
+
+// features          Array                                     Tupple
+// type         Homogeneours [same type of elements]       Hetrogeneours [diff type]
+// length         Dynamic [grow/shrink]                     Fixed
+// order            Not enforced                           Order is enforced
+//                                                      (specific type at each index)
+
+let user: [number, string, boolean] = [12, 'bhanu', true];
