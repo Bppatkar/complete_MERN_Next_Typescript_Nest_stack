770bf203db58824ba559a47e52601f8bad636b2f
Bhanu Pratap Patkar <bhanupratappatkar777@gmail.com>
2025-08-26 10:40:33 +0530

    creating small todo proect using typescript

---
+++ my_learning/NodeJsInDepthForJob/11. nodejs-with-typescript/src/app.ts
@@ -0,0 +1,12 @@
+// import express from 'express';
+
+// const app = express();
+// const port = 2332;
+
+// app.use(express.json());
+
+// app.listen(port, () => {
+//   console.log(`server is running on http://localhost:${port}`);
+// });
+
+console.log('Hello from ts file');
