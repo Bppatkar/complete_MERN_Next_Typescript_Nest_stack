cf81f0d69acfbefcae9e785034f104e32f83ede3
Bhanu Pratap Patkar <bhanupratappatkar777@gmail.com>
2025-09-03 09:50:11 +0530

    using express-http-proxy for correct incoming route and rate-limit for protect misuse of api

--- my_learning/NodeJsInDepthForJob/part2/social-microservices/api-gateway/src/server.js
+++ my_learning/NodeJsInDepthForJob/part2/social-microservices/api-gateway/src/server.js
@@ -1,11 +1,14 @@
+// express-http-proxy --> to route incoming requests to the correct service (Identity, Posts, Media, etc.).
+// express-rate-limit and rate-limit-redis  --> to protect the API from misuse by limiting the number of requests a user can make.
+
 import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'corss';
 import Redis from 'ioredis';
 import helmet from 'helmet';
+import logger from './utils/logger';
 import { rateLimit } from 'express-rate-limit';
 import { RedisStore } from 'rate-limit-redis';
-import logger from './utils/logger';
 import proxy from 'express-http-proxy';
 import errorHandler from './middleware/errorhandler';
 import validToken from './middleware/authMiddleware';
