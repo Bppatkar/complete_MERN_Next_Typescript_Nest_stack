dd9173405323d628aa9229ea8ea6c1bbe86af794
Bhanu Pratap Patkar <bhanupratappatkar777@gmail.com>
2025-09-04 21:50:54 +0530

    search service start

--- my_learning/NodeJsInDepthForJob/part2/social-microservices/media-service/src/server.js
+++ my_learning/NodeJsInDepthForJob/part2/social-microservices/media-service/src/server.js
@@ -9,6 +9,86 @@ import errorHandler from './middleware/errorHandler.js';
 import logger from './utils/logger.js';
 import { connectToRabbitMQ, consumeEvent } from './utils/rabbitmq.js';
 import { handlePostDeleted } from './eventHandlers/media-event-handlers.js';
+import { RateLimiterRedis } from 'rate-limiter-flexible';
+import { rateLimit } from 'express-rate-limit';
 
 const app = express();
 const PORT = process.env.PORT || 3003;
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => logger.info('Connected to mongodb'))
+  .catch((e) => logger.error('Mongo connection error', e));
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+
+app.use((req, res, next) => {
+  logger.info(`Received ${req.method} request to ${req.url}`);
+  logger.info(`Request body, ${req.body}`);
+  next();
+});
+
+const rateLimiter = new RateLimiterRedis({
+  storeClient: redisClient, // Redis client instance to store rate limit data
+  keyPrefix: 'middleware', // Prefix added to Redis keys to distinguish rate limiting data
+  points: 10, // Maximum number of requests allowed from a single IP in the duration period
+  duration: 1, // Time window in seconds during which the points are counted (1 second)
+});
+app.use((req, res, next) => {
+  rateLimiter
+    .consume(req.ip)
+    .then(() => next())
+    .catch(() => {
+      logger.warn(`Rate limit exceeded for IP:${req.ip}`);
+      res.status(429).json({ success: false, message: 'To many requests' });
+    });
+});
+
+const sensitiveEndpointsLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 50,
+  standardHeaders: true,
+  legacyHeaders: false,
+  handler: (req, res) => {
+    logger.warn(`Sensitive endpoint rate limit exceeded for IP:${req.ip}`);
+  },
+  store: new RedisStore({
+    sendCommand: (...args) => redisClient.call(...args),
+  }),
+});
+
+// apply this sensitiveEndpointsLimiter to our routes
+app.use('/api/auth/register', sensitiveEndpointsLimiter);
+
+app.use('/api/media', mediaRoutes);
+
+app.use(errorHandler);
+
+async function startServer() {
+  try {
+    await connectToRabbitMQ();
+
+    //consume all the events
+    await consumeEvent('post.deleted', handlePostDeleted);
+
+    app.listen(PORT, () => {
+      logger.info(`Media service running on port ${PORT}`);
+    });
+  } catch (error) {
+    logger.error('Failed to connect to server', error);
+    process.exit(1);
+  }
+}
+
+startServer();
+
+app.listen(PORT, () => {
+  logger.info(`Idendity service running on port ${PORT}`);
+});
+
+// unhandled promise rejection
+process.on('unhandledRejection', (reason, promise) => {
+  logger.error('Unhandled Rejection at', promise, 'reason:', reason);
+});
