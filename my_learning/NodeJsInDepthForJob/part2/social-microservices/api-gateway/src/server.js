719f37ef3d3fe4e7b6594e7ab64712afc508cbc6
Bhanu Pratap Patkar <bhanupratappatkar777@gmail.com>
2025-09-05 17:46:53 +0530

    minor changes while checking api in postman

--- my_learning/NodeJsInDepthForJob/part2/social-microservices/api-gateway/src/server.js
+++ my_learning/NodeJsInDepthForJob/part2/social-microservices/api-gateway/src/server.js
@@ -17,13 +17,15 @@ import validToken from './middleware/authMiddleware.js';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const redisClient = new Redis(process.env.REDIS_URI);
+const redisClient = new Redis(
+  process.env.REDIS_URI || 'redis://localhost:6379'
+);
 
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
 
-// rate limitting
+// rate limitting [very basic rate limit but for advanced we use rate-limitter-flexible]
 const rateLimitOptions = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
@@ -49,9 +51,12 @@ app.use((req, res, next) => {
   next();
 });
 
+//! setting up proxy for our identity service [to route incoming requests to the correct service]
+//! how to set proxy [go npm package and search these two] --> 1.proxyReqPathResolver and 2. proxyErrorHandler
 const proxyOptions = {
   proxyReqPathResolver: (req) => {
     return req.originalUrl.replace(/^\/v1/, '/api');
+    // it will replace v1 from api --> localhost:3000/v1/auth/register ===> to===> localhost:3000/api/auth/register
   },
   proxyErrorHandler: (err, res, next) => {
     logger.error(`Proxy error: ${err.message}`);
@@ -62,12 +67,18 @@ const proxyOptions = {
   },
 };
 
-//! setting up proxy for our identity service
-//* skeleton
+//! setting up proxy for authentication routes/Identity services
+//*** skeleton
 /*
-app.use('/v1/auth', validToken, proxy(process.env.POST_SERVICE_URL, {
-  ...proxyOptions,
-  proxyReqOptDecorator: (proxyReqOpts, srcReq) => {},
+app.use('/v1/auth', validToken [if needed], proxy(process.env.POST_SERVICE_URL, {
+  ...proxyOptions, //? destructuring the proxy options [above created object]
+  //? these options will allow customisation of proxy request before sent to the target
+  //? for more detail go npm search expres-http-proxy --> search [proxyReqOptDecorator]
+  //! Request [adding any imp thing in request and sent to user]
+  proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
+    proxyReqOpts.headers['Content-Type'] = 'application/json';
+      return proxyReqOpts;},
+  //! Response [getting response back from user]
   userResDecorator: (proxyRes, proxyResData, userReq, userRes) => {
     logger.info(`Response received from Identity service : ${proxyRes.statusCode}`);
     return proxyResData;
@@ -76,6 +87,7 @@ app.use('/v1/auth', validToken, proxy(process.env.POST_SERVICE_URL, {
  */
 app.use(
   '/v1/auth',
+  //? it will replace v1/auth from api --> localhost:3000/v1/auth/register ===> to ===> localhost:3000/api/auth/register
   proxy(process.env.IDENTITY_SERVICE_URL, {
     ...proxyOptions,
     proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
@@ -175,5 +187,5 @@ app.listen(PORT, () => {
   logger.info(
     `Search service is running on port ${process.env.SEARCH_SERVICE_URL}`
   );
-  logger.info(`Redis Url ${process.env.REDIS_URL}`);
+  logger.info(`Redis Url ${process.env.REDIS_URI}`);
 });
