f5ce9ddbea927868457ad8c6084eec149f8bc0a3
Bhanu Pratap Patkar <bhanupratappatkar777@gmail.com>
2025-09-10 17:32:19 +0530

    writting query about basic CRUD

--- my_learning/NodeJsInDepthForJob/part3/postgres/postgress-concept/src/main.js
+++ my_learning/NodeJsInDepthForJob/part3/postgres/postgress-concept/src/main.js
@@ -1,2 +1,42 @@
-import dotenv from 'dotenv';
-dotenv.config();
+import {
+  createUsersTable,
+  deleteInfo,
+  fetchAllUsers,
+  insertUser,
+  updateUserInfo,
+} from './concepts/basic-queries';
+
+async function testBasicQueries() {
+  try {
+    //! creating table
+    await createUsersTable();
+
+    //! inserting data in table [new users]
+    await insertUser('Zyx', 'zxy@gmail.com');
+    await insertUser('jhon Doe', 'jhon@gmail.com');
+    await insertUser('Travis MClaren', 'travis123@gmail.com');
+    await insertUser('Jennifer Lopez', 'jennifer@gmail.com');
+    await insertUser('Zayn Malik', 'zaynmalik@gmail.com');
+    console.log('All users');
+
+    //! all inserted data
+    const allUsers = await fetchAllUsers();
+    console.log(allUsers);
+
+    //! update data
+    const updatedUser = await updateUserInfo('Bhanu Pratap', 'bp@gmail.com');
+    console.log(updatedUser);
+
+    //! deleting data
+    const deleteUser = await deleteInfo('Bhanu Pratap');
+    console.log(deleteUser);
+  } catch (error) {
+    console.error('Error', error);
+  }
+}
+
+async function testAllQueries() {
+  await testBasicQueries();
+}
+
+testAllQueries();
