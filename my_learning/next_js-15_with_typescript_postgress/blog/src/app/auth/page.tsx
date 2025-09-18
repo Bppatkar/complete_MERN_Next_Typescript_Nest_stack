eface9a01ba0104f10182348a21f590c175f467c
Bhanu Pratap Patkar <bhanupratappatkar777@gmail.com>
2025-09-18 21:43:26 +0530

    few things done now come to main part

--- my_learning/next_js-15_with_typescript_postgress/blog/src/app/auth/page.tsx
+++ my_learning/next_js-15_with_typescript_postgress/blog/src/app/auth/page.tsx
@@ -1,7 +1,9 @@
+import AuthLayout from '@/components/auth/auth-layout';
+
 export default function AuthPost() {
   return (
     <div>
-      <h1>Auth Page - Login & Registration</h1>
+      <AuthLayout />
     </div>
   );
 }
